e8bc62a774609913a00b3978aecf1dc02de09696
Yi Kuo <kuokuoyiyi@gmail.com>
2020-03-25 14:41:59 +0800

    Data: Clean up old data method

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +12,6 @@ import (
 )
 
 var conf config
-var cards = make(map[string]card)
 
 var db *sql.DB
 
@@ -27,37 +25,6 @@ func loadConf(file string) {
 	}
 	jsonParser := json.NewDecoder(configFile)
 	jsonParser.Decode(&conf)
-}
-
-func loadCards(file string) {
-	cardsFile, err := os.Open(file)
-	defer cardsFile.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	var cardsJSON []card
-	json.NewDecoder(cardsFile).Decode(&cardsJSON)
-
-	for _, c := range cardsJSON {
-		cards[c.UID] = c
-	}
-}
-
-func saveCards(file string) {
-	cardsFile, err := os.Open(file)
-	defer cardsFile.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	var cardsArray []card
-	for _, c := range cards {
-		cardsArray = append(cardsArray, c)
-	}
-
-	JSONString, _ := json.MarshalIndent(cardsArray, "", " ")
-	ioutil.WriteFile(file, JSONString, os.ModePerm)
 }
 
 func loadDatabase(file string) {
@@ -77,8 +44,6 @@ func loadDatabase(file string) {
 func main() {
 	loadConf("config.json")
 	loadDatabase("cards.db")
-	loadCards("cards.json")
-	defer saveCards("cards.json")
 
 	gsheetsInit()
 
